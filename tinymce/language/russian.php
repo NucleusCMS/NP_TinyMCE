d56a7312bda4f66d779e67c963482afcbea3f30d
yama <yamamoto@kyms.ne.jp>
2011-09-03 21:48:48 +0900

    Add language files. arabic.php, chinese-gbk.php, chinese-utf8.php,french-utf8.php, french.php, german.php, polish.php,russian.php.

---
+++ tinymce/language/russian.php
@@ -0,0 +1,46 @@
+<?php
+// NP_TinyMCE
+
+// Description
+define('_NP_TINYMCE01',   'Makes it possibe to use TinyMCE (a WYSIWYG XHTML 1.0 editor) with Nucleus CMS. MSIE, FireFox and Safari.');
+
+// Member Option
+define('_NP_TINYMCE02',   'Use TinyMCE WYSIWYG editor.');
+define('_NP_TINYMCE03',   'Use custom setting, this member.');
+define('_NP_TINYMCE_PLUGIN_MEMBERCUSTOM_LABEL',   'TinyMCE Plugin (Custom setting)');
+define('_NP_TINYMCE_TOOLBAR_1_MEMBERCUSTOM_LABEL',   'ToolBar 1 (Custom setting)');
+define('_NP_TINYMCE_TOOLBAR_2_MEMBERCUSTOM_LABEL',   'ToolBar 2 (Custom setting)');
+define('_NP_TINYMCE_TOOLBAR_3_MEMBERCUSTOM_LABEL',   'ToolBar 3 (Custom setting)');
+define('_NP_TINYMCE_TOOLBAR_4_MEMBERCUSTOM_LABEL',   'ToolBar 4 (Custom setting)');
+
+// Global Option
+define('_NP_TINYMCE_TEXTAREA_WIDTH',   'textarea width(% or px)');
+define('_NP_TINYMCE_ENTERKEY_MODE_LABEL',   'Enter Key Action');
+define('_NP_TINYMCE_ENTERKEY_MODE_VALUE',   '<p></p> (BR, Shift+Enter)|p|insert <br />|br');
+define('_NP_TINYMCE_DIALOG_TYPE_LABEL',   'Popup dialog style');
+define('_NP_TINYMCE04',   'Extended valid elements. @*[*] unlimited');
+define('_NP_TINYMCE04_VALUE', 'admit only a safe html tag|safety|It is unlimited|unlimited');
+define('_NP_TINYMCE05',   'Use TinyMCE GZip compression (experimental).');
+define('_NP_TINYMCE06',   'The link is put by the relative path description from the document route.');
+define('_NP_TINYMCE07',   'some accessibility warnings will be presented to the user if they miss specifying that information.');
+define('_NP_TINYMCE08',   'Toolbar skin select');
+define('_NP_TINYMCE09',   'Default|default|o2007|o2k7|o2007(silver)|o2k7_silver|o2007(black)|o2k7_black');
+define('_NP_TINYMCE10',   'URL(http:..)|url|absolute(/xxx.jpg etc.)|absolute|relative|relative');
+
+define('_NP_TINYMCE_PLUGIN_LABEL',   'TinyMCE Plugin');
+define('_NP_TINYMCE_TOOLBAR_1_LABEL',   'ToolBar 1');
+define('_NP_TINYMCE_TOOLBAR_2_LABEL',   'ToolBar 2');
+
+define('_NP_TINYMCE_CUSTOM_CSS',   'custom class');
+
+define('_NP_TINYMCE_THEME_ADVANCED_STYLES',   'Label of custom class');
+define('_NP_TINYMCE_THEME_ADVANCED_STYLES_DEFAULTVALUE',   'Image on Left=justifyleft;Image on Right=justifyright');
+define('_NP_TINYMCE_DETECT_MODE_LABEL',   'enable area');
+define('_NP_TINYMCE_DETECT_MODE_VALUE',   'body and more|exact|all textareas|textareas');
+define('_NP_TINYMCE_TRIM_PTAG',   'Delete <p>&nbsp;</p>');
+define('_NP_TINYMCE_DEF_DIR_MODE', 'Default Directory Mode');
+define('_NP_TINYMCE_DEF_DIR_MODE_VALUE', 'per member|member|custom|fix');
+define('_NP_TINYMCE_DEF_DIR_VALUE', 'default directory (specified)');
+define('_NP_TINYMCE_INIT',   'Configuration');
+define('_NP_TINYMCE_INCLUDE_CSS',   'include css in <head>');
+define('_NP_TINYMCE_LANG_CODE',   'ru');
